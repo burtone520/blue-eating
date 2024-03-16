f61ac230c7902819f121da54db688b03a5b9e46c
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-03-16 16:39:27 -0400

    Navbar Current

---
+++ src/Navbar/navbar.jsx
@@ -0,0 +1,34 @@
+const Navbar = ({currPage,isLoggedIn}) =>{
+    getUsersName = ()=>{
+        return "E"
+    }
+return(
+    <div className="container navbar">
+        <img src="" alt="" />
+        {isLoggedIn!=""?
+        <ul className="navbar-list">
+            <li>Home</li>
+            <li>Life Style</li>
+            <li>Sign Up</li>
+        </ul>
+        :
+    <ul className="navbar-list">
+        <li>Login</li>
+        <li>Explore</li>
+        <li>Search Recipes</li>
+        <li>Create Recipe</li>
+        <li>Life Style</li>
+        <div>
+        <p>{getUsersName}</p>
+        <ul>
+            <li>Saved Recipes</li>
+            <li>Created Recipes</li>
+            <li>Sign Out</li>
+        </ul>
+        </div>
+    </ul>
+}
+    </div>
+)
+}
+export default Navbar;
