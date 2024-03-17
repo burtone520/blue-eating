748800ec448e9169a50c42e9bb48580463a3cf22
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-03-16 23:14:03 -0400

    updates

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,14 +1,17 @@
 import "./navbar.scss"
 import logo from "../blue eating logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBoxesAlt, faCaretDown, faCarrot } from '@fortawesome/free-solid-svg-icons'
 const Navbar = ({currPage,isLoggedIn}) =>{
+
     const getUsersName = ()=>{
         return "E"
     }
-    //create a 
 return(
     <div className="container-navbar">
         <img src={logo} alt="" className="logo"/>
-        {isLoggedIn === "false"?
+        {isLoggedIn === ""?
         <ul className="navbar-list">
             <li id={currPage=="home"?"curr":""}>Home</li>
             <li id={currPage=="lifeStyle"?"curr":""}>Life Style</li>
@@ -20,13 +23,16 @@ return(
         <li>Search Recipes</li>
         <li>Create Recipe</li>
         <li>Life Style</li>
-        <div>
-        <p>{getUsersName}</p>
+    
+        <div className="user-Dropdown">
+        <p className="userIcon">J</p>
+        <FontAwesomeIcon icon={faCaretDown} className="carretIcon" />
         <ul>
             <li>Saved Recipes</li>
             <li>Created Recipes</li>
             <li>Sign Out</li>
         </ul>
+  
         </div>
     </ul>
 }
