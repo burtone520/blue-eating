ebcbb446c4f84c1b1de2e43263455a7aa762eb50
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-03-17 08:16:03 -0400

    Fuck you too

--- src/display-recipe/RecipeDisplay.jsx
+++ src/display-recipe/RecipeDisplay.jsx
@@ -1,5 +1,7 @@
 import "./RecipeDisplay.scss"
 import { useParams } from "react-router-dom";
+import Navbar from "../Navbar/navbar";
+import React from "react";
 const RecipeDisplay = () =>{
     const { id } = useParams(); // Access the "id" parameter from the URL
     const randomNumber = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
@@ -11,17 +13,19 @@ const RecipeDisplay = () =>{
         "instructions":"Place rice in a fine sieve. Rinse under cold water until water is nearly clear, about 1 minute. Drain well then set aside.Melt 1 Tbsp butter in a pot over medium heat. Add onion and saute 4 minutes. Add garlic and saute 30 seconds longer.Pour in chicken broth, lemon zest and season with salt and pepper to taste. Bring to a boil, then pour in rice and give it a quick stir. Cover and reduce heat to low. Let simmer 13 minutes (meanwhile peel the shrimp to make the most of the time). After 13 minutes quickly add the shrimp to the rice in an even layer, drizzle with lemon juice, dot with remaining 4 Tbsp butter. Cover and continue to cook 2 to 3 minutes longer until shrimp are about halfway cooked. Remove from heat. Toss in parmesan and parsley. Let rest for a few minutes, covered until shrimp are fully cooked through from the residual heat."
     }
 return(
+    <React.Fragment>
+        <Navbar isLoggedIn={true}/>
     <div className="Container">
         <div className="Image-Container">
     <img src={demoRecipe.image} alt="" />
         </div>
         <div className="Recipe-details">
-            <h1>{demoRecipe.Title}</h1>
-            <p>Cooking time: {randomNumber}</p>
-            <p>Creator:{demoRecipe.Creator}</p>
+            <h1 className="Recipe-Title">{demoRecipe.Title}</h1>
+            <p className="cookingTime">Cooking time: {randomNumber}</p>
+            <p className="creator">Creator:{demoRecipe.Creator}</p>
         </div>
         <div className="Recipe-Ingredients">
-            <p>Ingredients:</p>
+            <p className="IngredientsHeader">Ingredients:</p>
             {demoRecipe.ingredientList!=undefined&& demoRecipe.ingredientList.map(item=>{
                 return(
                     <p className="ingredientItem">
@@ -30,13 +34,15 @@ return(
                 )
             })}
         </div>
+        </div>
         <div className="Recipe-Directions">
-            <h1>Directions:How to Make {demoRecipe.Title}</h1>
+            <h1>Directions: How to Make {demoRecipe.Title}</h1>
             <p className="Directions-txt">
             {demoRecipe.instructions}
             </p>
         </div>
-    </div>
+    
+    </React.Fragment>
 )
 }
 export default RecipeDisplay;
