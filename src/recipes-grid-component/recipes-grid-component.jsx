abbc8d38fd667e230ffa87931b8f8e5461d608f0
eburton520 <eb953362@wcupa.edu>
2024-03-17 10:50:01 -0400

    recipe block

--- src/recipes-grid-component/recipes-grid-component.jsx
+++ src/recipes-grid-component/recipes-grid-component.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import "./recipes-grid-component.scss";
+import Recipe_Block from '../Recipe_Block';
 
 const RecipesGrid = ({recipies}) => {
+    
     return (
         <div className="grid-container">
-            {recipies.length!=undefined&&recipies.map((recipe)=>{
+            {recipies!=undefined && recipies.map((recipe)=>{
                 return(
                     <div className="grid-item">
-
+                        <Recipe_Block>
+                            {recipe.name}
+                            Minutes: {recipe.time}
+                        </Recipe_Block>
                     </div>
                 )
             })}
-            <div className="grid-item">1</div>
-            <div className="grid-item">2</div>
-            <div className="grid-item">3</div>
-            <div className="grid-item">4</div>
-            <div className="grid-item">5</div>
-            <div className="grid-item">6</div>
-            <div className="grid-item">7</div>
-            <div className="grid-item">8</div>
-            <div className="grid-item">9</div>
-            <div className="grid-item">10</div>
-            <div className="grid-item">11</div>
-            <div className="grid-item">12</div>
         </div>
     );
 }; export default RecipesGrid;
