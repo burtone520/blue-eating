404e7b7e1a71e9fd24e9f44bce19a94f5596557d
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-03-17 02:05:32 -0400

    Updated Login Page

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -4,23 +4,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBoxesAlt, faCaretDown, faCarrot } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
-const Navbar = ({currPage,isLoggedIn}) =>{
-
+import { useState } from "react"
+const Navbar = ({currPage,isLoggedIn,toggleLogin}) =>{
+    const [displayLogin,setDisplayLogin] = useState(false)
     const getUsersName = ()=>{
         return "E"
     }
 return(
     <div className="container-navbar">
         <img src={logo} alt="" className="logo"/>
-        {isLoggedIn === ""?
+        {isLoggedIn === true?
         <ul className="navbar-list">
-            <li id={currPage=="home"?"curr":""}><Link to="/">Home</Link></li>
+            <li id={currPage=="home"?"curr":""}><Link to="/" style={{ textDecoration: 'none', color:"inherit"}}>Home</Link></li>
             <li id={currPage=="lifeStyle"?"curr":""}>Life Style</li>
-            <li>Sign Up</li>
+            <li onClick={()=>toggleLogin()}>Sign Up</li>
         </ul>
         :
     <ul className="navbar-list">
-        <li><Link to="recipies">Explore</Link></li>
+        <li><Link to="recipies" style={{ textDecoration: 'none', color:"inherit"}}>Explore</Link></li>
         <li>Search Recipes</li>
         <li>Create Recipe</li>
         <li>Life Style</li>
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser, faLock, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import logo from "../blue eating logo.svg";
-
-const Login = () => {
+import "./login.scss"
+const Login = ({toggleDisplay}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isSignUp, setSignUp] = useState(false);
@@ -13,23 +13,30 @@ const Login = () => {
         setUsername("");
         setSignUp(!isSignUp);
     };
+    const closeDisplay = () =>{
+        toggleDisplay()
+    }
 
     return (
         <div className="signin-container"> 
             <img src={logo} alt="" className="logo"/>
-            <FontAwesomeIcon icon={faCircleXmark} className="closeIcon"/>
+            <FontAwesomeIcon icon={faCircleXmark} className="closeIcon" onClick={closeDisplay}/>
             <div className="input-box">
-                <FontAwesomeIcon icon={faCircleUser} />
+                <FontAwesomeIcon icon={faCircleUser} className="icon" />
                 <input type="text" onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
             </div>
             <div className="input-box">
-                <FontAwesomeIcon icon={faLock} />
+                <FontAwesomeIcon icon={faLock} className="icon"/>
                 <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             </div>
             {isSignUp ? 
-                <p>Already have an account?<button onClick={toggleSignUp}>Login</button></p> :
-                <p>Don’t have an account?<button onClick={toggleSignUp}>Sign Up</button></p>
+                <p className="btm-txt">Already have an account?<button onClick={toggleSignUp} id="secondary-btn">Login</button></p> :
+                <p className="btm-txt">Don’t have an account?<button onClick={toggleSignUp} id="secondary-btn">Sign Up</button></p>
             }
+            {isSignUp?
+           <button className="primary-btn">Sign up</button>:
+           <button className="primary-btn">Login</button>
+        }
         </div>
     );
 };
