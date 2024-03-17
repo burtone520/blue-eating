c418f802da5c407aa7e74187d741cefd126dae40
eburton520 <eb953362@wcupa.edu>
2024-03-17 02:08:49 -0400

    update

--- src/recipe-photos/recipes.jsx
+++ src/recipe-photos/recipes.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./recipes.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSearch} from "@fortawesome/free-solid-svg-icons"
 const Recipes = ({ id, time, image, recipeName }) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const handleFocus = () => {
+    setIsFocused(true);
+    };
   return (
     <div>
-        <div>
-            <div className="input">
-                <FontAwesomeIcon icon={faSearch} />
-                <input
-                    type="text"
-                    class="input-text"
-                    placeholder="Filter by ingredients"/>
-            </div>
+        <div className="input">
+            <FontAwesomeIcon icon={faSearch} class="search"/>
+            <input
+                type="text"
+                class={isFocused ? "input-with-no-border focused input-text placeholder" : "input-with-no-border input-text placeholder"} // Apply different classes based on focus state
+                onFocus={handleFocus}
+                placeholder="Filter by ingredients"/>
         </div>
         {/* <img class="recipes" src={image} alt={recipeName} />
         <div class="overlay-text">
