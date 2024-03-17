122b85219da75f457c8923fcb38fa986a7b52bf7
eburton520 <eb953362@wcupa.edu>
2024-03-17 00:58:07 -0400

    update

--- src/recipe-photos/recipes.jsx
+++ src/recipe-photos/recipes.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import "./recipes.scss";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faSearch} from "@fortawesome/free-solid-svg-icons"
 const Recipes = ({ id, time, image, recipeName }) => {
   return (
     <div>
-        <img class="recipes" src={image} alt={recipeName} />
+        <div>
+            <div className="input">
+                <FontAwesomeIcon icon={faSearch} />
+                <input
+                    type="text"
+                    class="input-text"
+                    placeholder="Filter by ingredients"/>
+            </div>
+        </div>
+        {/* <img class="recipes" src={image} alt={recipeName} />
         <div class="overlay-text">
             <h2 class="recipe-name">{recipeName}</h2>
             <h2 class="recipe-time">{time} minutes</h2>
-        </div>
+        </div> */}
       </div>
   );
 };
