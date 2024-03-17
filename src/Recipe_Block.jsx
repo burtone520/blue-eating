9d92b529c5ed449f2049620e59302d7be54b3897
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-03-17 11:18:34 -0400

    Added Reci

--- src/Recipe_Block.jsx
+++ src/Recipe_Block.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Recipe_Block.scss"
 const Recipe_Block = ({image,recipeName}) =>{
         const randomNumber = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
 return(
-    <React.Fragment>
+    <div className="Container">
     <img class="recipes" src={`data:image/jpeg;base64,${image}`} alt={recipeName} />
         <div class="overlay-text">
             <h2 class="recipe-name">{recipeName}</h2>
             <h2 class="recipe-time">{randomNumber} minutes</h2>
         </div> 
-        </React.Fragment>
+        </div>
 )
 }
 export default Recipe_Block
