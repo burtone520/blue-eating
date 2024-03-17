b5d5c4a2731107604eb22947a217bba5403f2706
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-03-17 02:48:58 -0400

    Finalized login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,12 +2,39 @@ import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser, faLock, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import logo from "../blue eating logo.svg";
+import axios from "axios";
 import "./login.scss"
 const Login = ({toggleDisplay}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isSignUp, setSignUp] = useState(false);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = {username:username,password:password}
+        console.log(formData)
+        if(isSignUp){
+
+        try {
+            const response = await axios.post('http://localhost:5231/register', formData);
+            console.log(await response.data); // Handle the response data here
+            setPassword("")
+            setUsername("")
+            closeDisplay()
+        } catch (error) {
+            console.error('Error:', error);
+        }}
+        else{
+            try {
+                const response = await axios.post('http://localhost:5231/login', formData);
+                console.log(await response.data); // Handle the response data here
+                setPassword("")
+                setUsername("")
+                closeDisplay()
+            } catch (error) {
+                console.error('Error:', error);
+            }}
+        }
     const toggleSignUp = () => {
         setPassword("");
         setUsername("");
@@ -23,19 +50,19 @@ const Login = ({toggleDisplay}) => {
             <FontAwesomeIcon icon={faCircleXmark} className="closeIcon" onClick={closeDisplay}/>
             <div className="input-box">
                 <FontAwesomeIcon icon={faCircleUser} className="icon" />
-                <input type="text" onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                <input type="text"  value={username} onChange={(e) => setUsername(e.target.value) && console.log(e.target.value)} placeholder="Username" />
             </div>
             <div className="input-box">
                 <FontAwesomeIcon icon={faLock} className="icon"/>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             </div>
             {isSignUp ? 
                 <p className="btm-txt">Already have an account?<button onClick={toggleSignUp} id="secondary-btn">Login</button></p> :
                 <p className="btm-txt">Don’t have an account?<button onClick={toggleSignUp} id="secondary-btn">Sign Up</button></p>
             }
             {isSignUp?
-           <button className="primary-btn">Sign up</button>:
-           <button className="primary-btn">Login</button>
+           <button className="primary-btn" onClick={handleSubmit}>Sign up</button>:
+           <button className="primary-btn" onClick={handleSubmit}>Login</button>
         }
         </div>
     );
